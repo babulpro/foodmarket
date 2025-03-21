7c1b6d4d3671bead191872d57d65fd18fdd891a6
babulpro <sh8657706@gmail.com>
2025-03-21 09:10:26 +0600

    fixed nabvar on the page

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,7 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import PlainNavbar from "@/lib/component/PlainNavbar";
-import MainNavbar from "@/lib/component/MainNavbar";
-import { cookies } from "next/headers";
+import MainNavbar from "@/lib/component/MainNavbar"; 
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -20,19 +19,14 @@ export const metadata = {
 };
 
 export default async function RootLayout({ children }) {
-
-   const cookieStore = await cookies()
-    const token = cookieStore.get('token')
+ 
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <div className="mb-20">
-          {token ? <PlainNavbar /> : <MainNavbar />}{
-                      token ? <PlainNavbar/> : <MainNavbar/>
-                    }
-          
+      
         </div>
         {children}
       </body>
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
  
+import MainNavbar from "@/lib/component/MainNavbar";
+import PlainNavbar from "@/lib/component/PlainNavbar";
+import { cookies } from "next/headers";
 import Image from "next/image";
 
-export default function Home() {
+export default async function Home() {
+     const cookieStore = await cookies()
+      const token = cookieStore.get('token')
   return (
+    <div>
+            {
+                      token ? <PlainNavbar/> : <MainNavbar/>
+                    }
+          
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
      
        Hello babul you are going to build an e-connerce prject.thank you so much.
+    </div>
     </div>
   );
 }
