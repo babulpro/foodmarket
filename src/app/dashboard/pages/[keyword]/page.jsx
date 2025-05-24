b78cd46d30a1227db07ae32299f72e2b7d505a03
babulpro <sh8657706@gmail.com>
2025-05-24 18:54:59 +0600

    ee

--- src/app/dashboard/pages/[keyword]/page.jsx
+++ src/app/dashboard/pages/[keyword]/page.jsx
@@ -1,37 +1,44 @@
- 
- 
-import FoodCard from "@/app/lib/component/utilityCom/FoodCard";
+"use client"
+import { useEffect, useState } from 'react';
+import FoodCard from '@/app/lib/component/utilityCom/FoodCard';
 
- 
- 
-
- 
-const getData =async(keyword)=>{
-    const config = {
-                method: 'GET',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                // body: JSON.stringify(postData)
-            };
-    let data =  await fetch(`http://localhost:3000/api/getData/product/allProduct/${keyword}`,{config})
-    const res = await data.json();
-    return res.data;
-}
-
-
-
-
-const Page=async({params})=>{
-    const {keyword} =await params;
-    const data = await getData(keyword)
+const Page = ({ params }) => {
     
-    return(
-        <div className="mt-16"> 
-           
+    const [data, setData] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const { keyword } =await params;
+            try {
+                const config = {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                };
+                const response = await fetch(`http://localhost:3000/api/getData/product/allProduct/${keyword}`, config);
+                const { data } = await response.json();
+                setData(data);
+                setLoading(false);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+                setLoading(false);
+            }
+        };
+
+        fetchData();
+    }, [1]);
+
+    if (loading) {
+        return <div className="mt-16">Loading...</div>;
+    }
+
+    return (
+        <div className="mt-16">
             <FoodCard items={data} />
         </div>
-    )
-}
+    );
+};
 
 export default Page;
